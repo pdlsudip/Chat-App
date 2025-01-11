da46a60d1ee947a28762955c1b5ba9fe767c9df1
Sudip <sudippaudel512@gmail.com>
2025-01-11 15:12:59 +0000

    Test

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,17 +6,17 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow your frontend domain
+    origin: ["http://localhost:5173", "http://127.0.0.1:5500"], // Allow your frontend domain
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies or credentials to be sent
-  })
+  }),
 );
 app.use(cookieParser());
 import authRouter from "./routes/auth.routes";
-import userRouter from "./routes/user.routes"
+import userRouter from "./routes/user.routes";
 import messageRouter from "./routes/message.routes";
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/messages", messageRouter)
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/messages", messageRouter);
+app.use("/api/v1/users", userRouter);
 export default app;
